6fa103ee0cb3bc0ac0841801917dfd65229cfd13
DanielKostura <danielkostura1@gmail.com>
2023-11-09 18:45:52 +0100

    Scikit learn - nedokoncene

--- 1. vlna/Scikit learn/None.py
+++ 1. vlna/Scikit learn/None.py
@@ -25,13 +25,29 @@ def predict_samples(clf: tree.DecisionTreeClassifier(),
     for i in range(len(wine_parameters)):
         if len(wine_parameters[i]) == 4:
             ok += 1
+
     if ok == len(wine_parameters):
         pole = list(clf.predict(wine_parameters))
         return pole
     return
 
+def predict_samples_add_none(clf: tree.DecisionTreeClassifier(), 
+                    wine_parameters: list[list[float]]) -> Optional[list[int]]:
+    pole = []
+    for i in range(len(wine_parameters)):
+        if len(wine_parameters[i]) == 4:
+            answer = clf.predict([wine_parameters[i]])
+            pole.append(answer[0])
+        elif len(wine_parameters[i]) != 4 and len(wine_parameters) != 1:
+            pole.append(None)
+        else:
+            return None
+        
+    return pole
+
 
 # Testy:
 clf = create_wine_tree()
 print(predict_samples(clf, [[12.8, 0.76, 79.3, 0.38], [13.5, 2.38, 103.5, 2.41]]))    # [1, 0]
+print(predict_samples(clf, [[12.8, 0.76, 79.3, 0.38], [13.5, 2.38, 103.5, 2.41], [11.6, 3.15, 114.3]])) # None / [1, 0, None]
 print(predict_samples(clf, [[11.6, 3.15, 114.3]]))    # None
