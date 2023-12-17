792ad4a226e62d49a193f4e80cee5a7d62d22315
DanielKostura <danielkostura1@gmail.com>
2023-12-17 10:06:32 +0100

    Update BGR→CMY.py

--- 2. vlna/Reprezentace_obrazu_v_pocitaci/BGR→CMY.py
+++ 2. vlna/Reprezentace_obrazu_v_pocitaci/BGR→CMY.py
@@ -2,16 +2,8 @@ import numpy as np
 
 # implement this
 def bgr_to_cmy(bgr_image):
-    r = bgr_image.copy()
-    for i in range(len(bgr_image)):
-        for j in range(len(bgr_image[i])):
-            bgr_image[i, j] = 255 - bgr_image[i, j]
-    
-    for i in range(len(bgr_image)):
-        for j in range(len(bgr_image[i])):
-            bgr_image[i, j] = np.flip(bgr_image[i, j])
+    pass
 
-    return r
 
 # test
 bgr_img = np.array([[[255,255,0],[150,50,0],[96,69,180]]])
